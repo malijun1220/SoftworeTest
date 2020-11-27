0010c0c6f3d4af78934a6f72a7cb03245e8c83a9
malijun <1318237250@qq.com>
2020-11-27 10:53:43 +0800

    修改，注入错误

--- src/main/java/Function.java
+++ src/main/java/Function.java
@@ -9,9 +9,11 @@ public class Function {
     private int totalTest;
 
     public Function(int totalTest) {
-        if (totalTest <= 1000) {
-            throw new IllegalArgumentException("totalTest must be larger than 300!" +
+        if (totalTest <= 500) {  //数据太小，样本不构成普遍性，跑出异常
+            this.totalTest = 0;
+            throw new IllegalArgumentException("totalTest must be larger than 1000!" +
                     "otherwise it's not statistically significant!");
+
         }
 
         this.totalTest = totalTest;
@@ -35,8 +37,8 @@ public class Function {
     private boolean isChangeDoor(boolean changeDoor) {
         int prizeDoor = (int) (random() * 3); //产生随机数
         int playerChoice = (int) (random() * 3);
-        if (playerChoice != prizeDoor) { //注入错误 ==变!=
-            return !changeDoor ;
+        if (playerChoice != prizeDoor) {//注入错误， ==边!=，结果就是换门的得奖率变为不换门的得奖率,
+            return !changeDoor ;              //不过由于这里的结果是动态变化的，所以测试中不易下断言，只能通过肉眼观察
         } else {
             return changeDoor ;
         }
--- src/test/java/FunctionTest.java
+++ src/test/java/FunctionTest.java
@@ -6,33 +6,27 @@ import org.junit.Test;
  * @date 2020/11/26 - 22:08
  */
 public class FunctionTest {
-    @org.junit.Before
-    public void setUp() throws Exception {
-        System.out.println("INIT!");
-    }
-
-    @org.junit.After
-    public void tearDown() throws Exception {
-        System.out.println("After");
-    }
 
     @Test
     public void run1() {
-        Function test = new Function(  5000);
+        System.out.println("testrun1");
+        Function test = new Function(5000);
         test.run(false);
         System.out.println();
         test.run(true);
     }
     @Test
     public void run2(){
-        Function test = new Function(500);
+        System.out.println("testrun2");
+        Function test = new Function(300);
         test.run(false);
         System.out.println();
         test.run(true);
     }
     @Test
     public void run3() {
-        Function test = new Function(5000);
+        System.out.println("testrun3");
+        Function test = new Function(1000);
         test.run(true);
         System.out.println();
         test.run(false);
